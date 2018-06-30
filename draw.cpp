f24a6b2893a91b17486de74203aa2adf0f756566
wwiabs <wwiabs@qq.com>
2018-06-30 16:30:38 +0800

    Add files via upload

--- draw.cpp
+++ draw.cpp
@@ -1,10 +1,9 @@
 ﻿#include "image.h"
 
-
 //bresenham
 void draw_line(Image* img, uchar v, int x0, int y0, int x1, int y1)
 {
-	if (x0<0 || x0>(int)img->width - 1 || x1<0 || x1>(int)img->width - 1 || y0<0 || y0>(int)img->height - 1 || y1<0 || y1>(int)img->height - 1)
+	if (x0<0 || x0>=(int)img->width || x1<0 || x1>=(int)img->width || y0<0 || y0>=(int)img->height || y1<0 || y1>=(int)img->height)
 		return;
 	int dx = abs(x1 - x0), sx = x0<x1 ? 1 : -1;
 	int dy = -abs(y1 - y0), sy = y0<y1 ? 1 : -1;
@@ -25,7 +24,7 @@ void draw_line(Image* img, uchar v, int x0, int y0, int x1, int y1)
 
 void draw_circle(Image* img, uchar v, int xm, int ym, int r)
 {
-	if (xm - r<0 || xm + r>(int)img->width - 1 || ym - r<0 || ym + r>(int)img->height - 1)
+	if (xm < r || xm + r>=(int)img->width || ym < r || ym + r>=(int)img->height)
 		return;
 	int x = -r, y = 0, err = 2 - 2 * r; /* bottom left to top right */
 	do {
@@ -40,29 +39,16 @@ void draw_circle(Image* img, uchar v, int xm, int ym, int r)
 	} while (x < 0);
 }
 
-
-
-//void draw4BMline(uchar* p, int w, int h, Point2d p1, Point2d p2, double a, double b, double c, uchar v)
-//{
-//	Image img(p, (unsigned)w, (unsigned)h);
-//	draw_line(&img, p1, p2, a, b, c, v);
-//	memcpy(p, img.data, w*h);
-//}
-//
-//void draw4BMcirlce(uchar* p, int w, int h, Point2d c, double r, uchar v)
-//{
-//	Image img(p, (unsigned)w, (unsigned)h);
-//	draw_circle(&img, c, r, v);
-//	memcpy(p, img.data, w*h);
-//}
-
-void fill(Image* img, const Rect& rect, uchar v)
+//  p1p2 cross p3p4  p1(x1,y1), p2(x2,y2), p3(x3, y3), p4(x4, y4) 
+inline void crosspt(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4, double *x, double *y)
 {
-	if (rect.x < 0 || rect.y < 0 || rect.y + rect.height > (int)img->height || rect.x + rect.width > (int)img->width)
-		return;
-
-	for (int r = rect.y; r < rect.y + rect.height; r++)
-	{
-		memset(img->ptr(r, rect.x), v, rect.width);
-	}
+	double A = y2 - y1;
+	double B = x1 - x2;
+	double C = x1*y2 - x2*y1;
+	double D = y4 - y3;
+	double E = x3 - x4;
+	double F = x3*y4 - x4*y3;
+	double G = A*E - B*D;
+	*x = (C*E - B*F) / G;
+	*y = (A*F - C*D) / G;
 }
